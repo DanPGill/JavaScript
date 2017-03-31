5edd705d9085205c67bab59e52c91884dd27ca80
Dan <danpgill@gmail.com>
2017-03-31 11:39:18 +0100

    Javascript.js updated with fifth task

--- Javascript.js
+++ Javascript.js
@@ -11,3 +11,5 @@ console.log(lastName + ", " + firstName);
 lastNameFirstName("Richard", "Gill");
 var firstName = "Richard";
 console.log(firstName.length);
+var people = ["Jack", "John", "James"];
+console.log(people[0] + "\n" + people[1] + "\n" + people[2]);
