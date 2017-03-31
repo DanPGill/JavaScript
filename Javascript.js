3cc9d882ba2bd0fd77088a3c30ac96674ab07ef6
Dan <danpgill@gmail.com>
2017-03-31 11:28:59 +0100

    Updated Javascript.js with second task

--- Javascript.js
+++ Javascript.js
@@ -1,3 +1,7 @@
 var firstName = 'Richard';
 var lastName = 'Gill';
 console.log(firstName + " " + lastName);
+function firstNamelastName(firstName, lastName){
+	console.log(firstName + " " + lastName);
+}
+firstNamelastName("Daniel", "Gill");
